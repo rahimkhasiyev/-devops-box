4d2497f23bbf50b743f30040d9a7c62a36ad6287
rahimkhasiyev <54140591+rahimkhasiyev@users.noreply.github.com>
2022-06-22 23:58:11 +0400

    Delete install_cdktf.sh

--- install_cdktf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get -y install golang
-sudo npm install --global cdktf-cli@latest
